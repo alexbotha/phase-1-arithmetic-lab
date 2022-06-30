5995760380754ef03e1ecd9ac05be6938a2960dc
alexbotha <alexbotha@hotmail.co.uk>
2022-06-30 09:42:49 +0100

    commit tree 4484d448808d185ef3c377b1f665c7e1124d0f54

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 31
+const num2 = 2
+const multiply = (num1 * num2)
 
-// Write your code here
+let random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 4
+const num4 = 0
+const mod = (num3 + num4)
+
+let max = Math.max(20)
